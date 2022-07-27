b24a69e4fa7a5d4fedf1d59fbd08fb08c224a814
Swarnabh Kashyap <swarnabh202@gmail.com>
2022-07-26 23:42:35 -0400

    Added the delete node functionlity

--- Chapter2/LinkedList.py
+++ Chapter2/LinkedList.py
@@ -21,14 +21,35 @@ class LinkedList:
     temp = self.head
     while(temp.next):
       temp = temp.next
-    
     temp.next = new_node
+  
+  def delete_node(self,key):
+    temp = self.head
+    #if node to be removed is at the head.
+    if(temp is not None):
+      if(temp.val==key):
+        self.head = temp.next
+        temp = None
+        return
+    #for finding the previous node of the node to be deleted.
+    while(temp):
+      if(temp.val==key):
+        break
+      prev = temp
+      temp = temp.next
+
+    #if the value is not found.
+    if(temp==None):
+      return
+    prev.next = temp.next
+    temp = None
   
   def printList(self):
     temp = self.head
     while(temp):
       print(temp.val,end=' ')
       temp = temp.next
+    print()
 
 llist = LinkedList()
 llist.insert_at_end(6)
@@ -36,4 +57,8 @@ llist.insert_at_beginning(7)
 llist.insert_at_beginning(1)
 llist.insert_at_end(4)
 llist.insert_after_node(llist.head.next.next,8)
+llist.printList()
+llist.delete_node(6)
+llist.printList()
+llist.delete_node(1)
 llist.printList()
